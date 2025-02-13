ed5fe089a6d0dc0ab4491643a56b8edd4e38eea2
VesaXhelili1 <vesa.xhelili@student.uni-pr.edu>
2025-02-13 20:52:17 +0100

    korigjim

--- main.cpp
+++ main.cpp
@@ -117,8 +117,7 @@ void Logic(){
         default:
             break;
     }
-    //if (x > width || x<0 || y> height || y<0)
-    //gameOver = true;
+
     if(x >= width) x=0; else if(x < 0) x = width - 1;
     if(y >= height y=0); else if(y < 0) y = height - 1;
 
